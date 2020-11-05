58751ca0c4822b5830c7090b0c1ee7e82d9839ae
lgentaz <lucile.gentaz@gmail.com>
2020-11-05 12:22:07 +0100

    first attempt: reading and organizing symptoms

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -2,42 +2,53 @@ package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	
 	
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+		
+		File filepath = new File("symptoms.txt");
+		
+		List<String> results = new ArrayList<String>();
+									
+				
+		if (filepath != null) {
+			try {
+				BufferedReader reader = new BufferedReader (new FileReader(filepath));
+				String line = reader.readLine();
+				
+				while (line != null) {
+					results.add(line);
+					line = reader.readLine();
+				}
+				
+				reader.close();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
 		}
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		Collections.sort(results);
+		
+		Set singleSymptoms = new TreeSet(results);
+		
+		
+		Map<String, Integer> symptomFrequency = new HashMap<String, Integer>();
+		
+
+		for (Object s : singleSymptoms) {
+			String k = s.toString();
+			System.out.println(k);
+
+		}
+		
 	}
+
 }
+
+
