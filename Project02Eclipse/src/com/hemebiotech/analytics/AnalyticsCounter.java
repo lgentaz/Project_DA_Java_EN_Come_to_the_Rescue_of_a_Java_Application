e6336578d3e127c8fc09b77711633bbb94082e37
lgentaz <lucile.gentaz@gmail.com>
2020-11-05 16:31:23 +0100

    first attempt: results to output file

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,45 +1,40 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class AnalyticsCounter {
 	
-	
 	public static void main(String args[]) throws Exception {
+
 		
-	File filepath = new File("symptoms.txt");
-		
+		//Creating 
+		File filepath = new File("symptoms.txt");
+		File output = new File("results.out");
 		List<String> results = new ArrayList<String>();
+		Map<String, Integer> symptomFrequency = new TreeMap<String, Integer>();
 													
 		if (filepath != null) {
 			try {
 				BufferedReader reader = new BufferedReader (new FileReader(filepath));
 				String line = reader.readLine();
-				
 				while (line != null) {
 					results.add(line);
 					line = reader.readLine();
 				}
-				
 				reader.close();
-				
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		}
+		}	
 		
-		Collections.sort(results);
-		
-		Set singleSymptoms = new TreeSet(results);
-		
-		
-		Map<String, Integer> symptomFrequency = new TreeMap<String, Integer>();
-		
-
+		Set<String> singleSymptoms = new HashSet<String>(results);
+	
 		for (Object s : singleSymptoms) {
 			String k = s.toString();
 			int v = 0;
@@ -51,12 +46,26 @@ public class AnalyticsCounter {
 				}
 			}
 			symptomFrequency.put(k,v);
-
 		}
 		
-		System.out.println(symptomFrequency);
+		if (symptomFrequency != null) {
+			try {
+				BufferedWriter writer = new BufferedWriter (new FileWriter(output));
+				for (Map.Entry<String, Integer> entry : symptomFrequency.entrySet()) {
+					String line = entry.getKey() + " : " + entry.getValue();
+					writer.write(line);
+					writer.write(System.getProperty( "line.separator" ));
+				}
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}	
+		
+		
 		
 	}
+
 
 }
 
